d42907f99c99785a417f0cc865a3e7196873dc53
Max Braz <braz.max@gmail.com>
2017-11-07 10:50:44 -0800

    add sex and handleKeyPress for user hitting enter

--- client/src/components/GirlInput.jsx
+++ client/src/components/GirlInput.jsx
@@ -14,23 +14,31 @@ class GirlInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: ''
+      name: '',
+      sex: 'Female',
     }
 
-    this.search = this.search.bind(this);
+    this.add = this.add.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   onChange(e) {
     this.setState({
-      item: e.target.value
+      name: e.target.value
     });
   }
 
-  search() {
-    this.props.onSearch(this.state.item);
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.add();
+    }
+  }
+
+  add() {
+    this.props.addName(this.state.name, this.state.sex);
     this.setState({
-      item: ''
+      name: ''
     });
   }
 
@@ -41,10 +49,11 @@ class GirlInput extends React.Component {
       <input
         placeholder="Enter Name"
         style={style.input}
-        value={this.state.item}
+        value={this.state.name}
         onChange={this.onChange}
+        onKeyUp={this.handleKeyPress}
       />
-      <button onClick={this.search} style={style.button}> + </button>
+      <button onClick={this.add} style={style.button}> + </button>
     </div>
     )
   }
