1d78e43528a68855c28e8cf02b00bbbde1c16268
Max Braz <braz.max@gmail.com>
2017-11-07 10:51:35 -0800

    post/get working properly

--- server/index.js
+++ server/index.js
@@ -1,29 +1,37 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const parser = require('parse-address');
-const zipcodes = require('zipcodes');
-const Item = require('../db/index.js');
+const Name = require('../db/index.js');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './../client/dist')));
 
-app.post('/item', (req, res) => {
-  // post to db
-  res.send(JSON.stringify('Successful Post'));
+app.post('/name', (req, res) => {
+  let name = new Name({
+    name: req.body.name,
+    sex: req.body.sex,
+    votes: 0,
+    vetoed: false,
+  });
+  name.save((err, name) => {
+    console.log('***************names saved');
+    if (err) {
+      console.log('err in post: ', err);
+    } else {
+      res.send(JSON.stringify('Successful Post'));
+    }
+  });
 });
 
-app.get('/items', (req, res) => {
-  // get from db
-
-  Item.find({}).limit(5).exec( (err, items) => {
+app.get('/names', (req, res) => {
+  Name.find((err, names) => {
     if (err) {
       console.log( 'server get request failure', err);
     } else {
       console.log('Success!');
     }
-    res.end(JSON.stringify(items));
+    res.end(JSON.stringify(names));
   });
 });
 
