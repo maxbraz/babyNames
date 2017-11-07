5c4d0e4a8bdb89fa19f588788fc1be23cfe672a8
Max Braz <braz.max@gmail.com>
2017-11-07 10:51:15 -0800

    add style and strikethrough for vetoed names

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const style = {
+  vetoed: {
+    textDecoration: 'line-through'
+  }
+}
 const ListItem = ({name}) => (
-  <li>
-    {`${name.name} ${name.sex} ${name.votes} ${name.vetoed}`}
-  </li>
+  name.vetoed ?
+    <li style={style.vetoed}>
+      {`${name.name}`}
+    </li>
+  :
+    <li>
+      {`${name.name}`}
+    </li>
 )
 
 export default ListItem;
+
+//${name.sex} ${name.votes} ${name.vetoed}
